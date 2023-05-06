6b96bb9c85763c40eec4dad405588194b25d4954
NEBULA1978 <rpascualey@gmail.com>
2023-05-06 14:32:58 +0200

    Añadiendo post/get/delete  users

--- routes/users.routes.go
+++ routes/users.routes.go
@@ -6,17 +6,36 @@ import (
 
 	"github.com/NEBULA1978/rest-api-postgreSQL/db"
 	"github.com/NEBULA1978/rest-api-postgreSQL/models"
+	"github.com/gorilla/mux"
 )
 
 func GetUsersHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("get users"))
+	var users []models.User
+	db.DB.Find(&users)
+	json.NewEncoder(w).Encode(&users)
 }
+
 func GetUserHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("get user"))
+	params := mux.Vars(r)
+	var user models.User
+	db.DB.First(&user, params["id"])
+
+	// Si no existe ususario por ID
+	if user.ID == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("User not found"))
+		return
+	}
+
+	db.DB.Model(&user).Association("Tasks").Find(&user.Tasks)
+
+	json.NewEncoder(w).Encode(&user)
 }
+
 func PostUserHandler(w http.ResponseWriter, r *http.Request) {
 	var user models.User
 	json.NewDecoder(r.Body).Decode(&user)
+
 	createdUser := db.DB.Create(&user)
 	err := createdUser.Error
 
@@ -27,6 +46,18 @@ func PostUserHandler(w http.ResponseWriter, r *http.Request) {
 
 	json.NewEncoder(w).Encode(&user)
 }
+
 func DeleteUserHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("delete"))
+	params := mux.Vars(r)
+	var user models.User
+	db.DB.First(&user, params["id"])
+
+	if user.ID == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("User not found"))
+		return
+	}
+
+	db.DB.Unscoped().Delete(&user)
+	w.WriteHeader(http.StatusOK)
 }
