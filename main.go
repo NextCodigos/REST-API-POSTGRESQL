a2d1dfa381e881bdfc4782aecabfb0636c31e169
NEBULA1978 <rpascualey@gmail.com>
2023-05-06 15:32:29 +0200

    Añadiendo post/get/delete y Task  users

--- main.go
+++ main.go
@@ -2,32 +2,46 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/NEBULA1978/rest-api-postgreSQL/db"
 	"github.com/NEBULA1978/rest-api-postgreSQL/models"
 	"github.com/NEBULA1978/rest-api-postgreSQL/routes"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// database connection
 	db.DBConnection()
 	// db.DB.Migrator().DropTable(models.User{})
 	db.DB.AutoMigrate(models.Task{})
 	db.DB.AutoMigrate(models.User{})
+
 	r := mux.NewRouter()
 
+	// Index route
 	r.HandleFunc("/", routes.HomeHandler)
 
+	s := r.PathPrefix("/api").Subrouter()
+
+	// tasks routes
+	s.HandleFunc("/tasks", routes.GetTasksHandler).Methods("GET")
+	s.HandleFunc("/tasks/{id}", routes.GetTaskHandler).Methods("GET")
+	s.HandleFunc("/tasks", routes.CreateTaskHandler).Methods("POST")
+
 	// users routes
-	r.HandleFunc("/users", routes.GetUsersHandler).Methods("GET")
-
-	r.HandleFunc("/users/{id}", routes.GetUserHandler).Methods("GET")
-
-	r.HandleFunc("/users", routes.PostUserHandler).Methods("POST")
-
-	r.HandleFunc("/users/{id}", routes.DeleteUserHandler).Methods("DELETE")
+	s.HandleFunc("/users", routes.GetUsersHandler).Methods("GET")
+	s.HandleFunc("/users/{id}", routes.GetUserHandler).Methods("GET")
+	s.HandleFunc("/users", routes.PostUserHandler).Methods("POST")
+	s.HandleFunc("/users/{id}", routes.DeleteUserHandler).Methods("DELETE")
 
 	// Inicia el servidor en el puerto 8080
 	fmt.Println("Servidor iniciado en http://localhost:8080")
--- models/Task.go
+++ models/Task.go
@@ -1,11 +1,60 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"encoding/json"
+	"net/http"
 
-type Task struct {
-	gorm.Model
-	Title       string `gorm:"not null;unique_index" json:"title"`
-	Description string `json:"description"`
-	Done        bool   `gorm:"default:false" json:"done"`
-	UserID      uint   `json:"user_id"`
+	"github.com/NEBULA1978/go-postgresql-restapi/models"
+
+	"github.com/NEBULA1978/rest-api-postgreSQL/db"
+	"github.com/gorilla/mux"
+)
+
+func GetTasksHandler(w http.ResponseWriter, r *http.Request) {
+	var tasks []models.Task
+	db.DB.Find(&tasks)
+	json.NewEncoder(w).Encode(&tasks)
+}
+
+func GetTaskHandler(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var task models.Task
+	db.DB.First(&task, params["id"])
+
+	if task.ID == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Task not found"))
+		return
+	}
+
+	json.NewEncoder(w).Encode(&task)
+}
+
+func CreateTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var task models.Task
+	json.NewDecoder(r.Body).Decode(&task)
+	createdTask := db.DB.Create(&task)
+	err := createdTask.Error
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+	}
+
+	json.NewEncoder(w).Encode(&task)
+}
+
+func DeleteTaskHandler(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var task models.Task
+	db.DB.First(&task, params["id"])
+
+	if task.ID == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Task not found"))
+		return
+	}
+
+	db.DB.Unscoped().Delete(&task)
+	w.WriteHeader(http.StatusOK)
 }
